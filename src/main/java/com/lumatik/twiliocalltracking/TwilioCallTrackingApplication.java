93c06b248c8fadaab44576dbb2fcecf4402101a8
Michael Gulley <mgulley.mograph@gmail.com>
2017-08-23 14:00:47 -0400

    Initial commit

---
+++ src/main/java/com/lumatik/twiliocalltracking/TwilioCallTrackingApplication.java
@@ -0,0 +1,12 @@
+package com.lumatik.twiliocalltracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwilioCallTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwilioCallTrackingApplication.class, args);
+	}
+}
